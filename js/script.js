13adeadf9d6e2c38d4d5153f24f364ebcef23171
Paolo Cannistraro <wasdgamer91@gmail.com>
2022-05-02 16:44:03 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        emailList: [],
+        loading: ''
+    },
+    methods:{ 
+
+        getData() {
+            this.emailList = [];
+            for (let i = 0; i < 10; i++) {
+                axios
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((res) => {
+                        this.email = (res.data.response)
+                        this.emailList.push(this.email)
+                        this.loading = false;
+                    })
+                    .catch((error) => {
+                        console.log(error)
+                    })
+                    .finally(() => {
+                        this.loading = false;
+                    })
+            }
+        }
+        
+    },
+    created() {
+        this.loading = true;
+        this.getData();
+    }
+})
